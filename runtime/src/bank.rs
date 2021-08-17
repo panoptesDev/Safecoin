04213c1387a4c39df303e592ba5804ee9d3c1e55
sol <safe@vmi504578.contaboserver.net>
2021-08-17 05:38:13 +0200

    reconfigure_token2_native_mint Epoch 61

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -5014,8 +5014,8 @@ impl Bank {
         let reconfigure_token2_native_mint = match self.cluster_type() {
             ClusterType::Development => true,
             ClusterType::Devnet => true,
-            ClusterType::Testnet => self.epoch() == 93,
-            ClusterType::MainnetBeta => self.epoch() == 75,
+            ClusterType::Testnet => self.epoch() == 10,
+            ClusterType::MainnetBeta => self.epoch() == 61,
         };
 
         if reconfigure_token2_native_mint {
